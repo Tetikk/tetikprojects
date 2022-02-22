d3c57afb4988543e146c58f9743d87ead78f8ea6
Tetikk <100219041+Tetikk@users.noreply.github.com>
2022-02-22 19:03:08 +0300

    atm

--- atm.py
+++ atm.py
@@ -1,35 +1,54 @@
+import time
 from tkinter import N
 from unicodedata import name
 a = 0
 sifre = 0
+odenecekkredi = 0
 while 1:
     if sifre == 0:
+        print("Tetik Bank'a Hoşgeldiniz")
         sifre = int(input("Bir Şifre Oluşturunuz\t:"))
         sifreonay = int(input("Şifreyi Tekrarlayınız\t:"))
     else:
         giris = int(input("Lütfen Şifrenizi Giriniz\t:"))
+        print("Giriş Yapılıyor")
+        time.sleep(2)
         if giris == sifre:
-            print("Para Çekmek İçin|1\nPara Yatırmak İçin|2\nBakiye Öğrenmek İçin|3\nÇıkış Yapmak İçin|0")
+            print("Para Çekmek İçin|1\nPara Yatırmak İçin|2\nBakiye Öğrenmek İçin|3\nKredi Çekmek İçin|4\nKredi Borcu Ödemek İçin|5\nKredi Borcu Sorgulamak İçin|6\nÇıkış Yapmak İçin|0\n")
             islem = int(input("Hangi İşlemi Yapmak İstersiniz\t:"))
             if islem == 1:
                 paracek = int(input("Çekeceğiniz Miktarı Giriniz\t:"))
                 if paracek==0 or paracek<0:
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
                     print("İşlem Geçersiz\n")
                 elif paracek>a:
-                    print("Hesabınızda Bu Kadar Miktar Bulunmuyor.\n")
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
+                    print("İşlem başarısız, Hesabınızda Bu Kadar Miktar Bulunmuyor.\n")
                 else:
                     a = a-paracek
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
                     print(paracek,"TL Para Çektiniz\n")
             elif islem == 2:
                 parayatir = int(input("Yatıracağınız Miktarı Giriniz\t:"))
                 if parayatir==0 or parayatir<0:
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
                     print("İşlem Geçersiz\n")
                 else:
                     a = a+parayatir
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
                     print(parayatir,"TL Para Yatırdınız\n")
             elif islem == 3:
+                print("İşlem Gerçekleştiriliyor.")
+                time.sleep(2)
                 print(a,"TL Bakiyeniz Bulunmakta\n")
             elif islem == 0:
+                print("Çıkış Yapılıyor.")
+                time.sleep(2)
                 break
             elif islem == 4:
                 print("Faiz %1")
@@ -38,23 +57,38 @@ while 1:
                 print("Ödencek Miktar",odenecekkredi)
                 b = int(input("Onaylıyor musunuz?(1:evet, 0:hayır)\t:"))
                 if b==1:
+                    print("İşlem Gerçekleştiriliyor.")
+                    time.sleep(2)
                     print("İşlem Gerçekleştirilmiştir.\n")
                     a = a+kredi
                 else:
                     print("")
             elif islem == 5:
-                c = int(input("Ödenecek Tutarı Giriniz\t:"))
-                if c>odenecekkredi:
-                    print("Girdiğiniz tutar borcunuzdan fazla.\n")
-                elif c<1:
-                    print("İşlem Geçersiz.")
-                elif odenecekkredi==0:
-                    odenecekkredi = odenecekkredi-c
-                    print("Kredi Borcunuz Kalmadı")
+                if odenecekkredi==0:
+                    print("Kredi Borcunuz Bulunmamaktadır.")
                 else:
-                    odenecekkredi = odenecekkredi-c
-                    print(odenecekkredi,"TL Borcunuz Kaldı")                    
+                    c = int(input("Ödenecek Tutarı Giriniz\t:"))
+                    if c>odenecekkredi:
+                        print("İşlem Gerçekleştiriliyor.")
+                        time.sleep(2)
+                        print("İşem başarısız, girdiğiniz tutar borcunuzdan fazla.\n")
+                    elif c<1:
+                        print("İşlem Gerçekleştiriliyor.")
+                        time.sleep(2)
+                        print("İşlem Geçersiz.")
+                    elif odenecekkredi==0:
+                        odenecekkredi = odenecekkredi-c
+                        print("İşlem Gerçekleştiriliyor.")
+                        time.sleep(2)
+                        print("Kredi Borcunuz Kalmadı")
+                    else:
+                        odenecekkredi = odenecekkredi-c
+                        print("İşlem Gerçekleştiriliyor.")
+                        time.sleep(2)
+                        print(odenecekkredi,"TL Borcunuz Kaldı")                    
             elif islem == 6:
+                print("Kredi Borcu Sorgulanıyor.")
+                time.sleep(2)
                 print(odenecekkredi,("TL Kredi borcunuz bulunmakta.\n"))
         else:
             print("Şifre Yanlış Lütfen Tekrar Deneyiniz.\n")
